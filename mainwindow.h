6575f3ba6ac5dd0af0e0727bd4a09bbaaa92765d
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2012-01-31 11:40:06 +0200

    Minor change

--- mainwindow.h
+++ mainwindow.h
@@ -27,8 +27,6 @@ private slots:
     void on_loadValue_valueChanged(double arg1);
     void on_length_valueChanged(double arg1);
     void on_material_currentIndexChanged(int index);
-    void on_width_valueChanged(double arg1);
-    void on_height_valueChanged(double arg1);
     void on_uniformLoad_clicked(bool checked);
     void on_singleLoad_clicked(bool checked);
 
